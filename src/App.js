6760a465c1575cf0a8b42dbd9d4c19fa39de0188
user18718722 <vortexxd5@gmail.com>
2023-09-11 22:01:48 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import{Route, Routes, BrowserRouter} from 'react-router-dom'
 import './App.css';
-
-function App() {
+import Home from './pages/Home';
+import About from './pages/about'
+import ContactMe from './pages/contactMe'
+import Header from './components/header'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <BrowserRouter>
+    <Header/>
+    <Routes>
 
-export default App;
+    <Route path="/" element={<Home/>} />
+    <Route path="about" element={<About/>} />
+    <Route path="contactMe" element={<ContactMe/>} />
+      
+    </Routes>
+</BrowserRouter>
+  )
+
+}
---
+++ src/pages/contactMe.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function contactMe() {
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default contactMe
