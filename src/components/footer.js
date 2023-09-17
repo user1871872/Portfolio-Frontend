cf62b2d0cfde690e90382ba8c8170e5f24394334
user18718722 <vortexxd5@gmail.com>
2023-09-17 13:03:07 +0800

    commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,8 +14,8 @@ export default function footer() {
                 </div>
                 <div id='contact' className='text-white pt-10 lg:pt-0'>
                     <p className='text-4xl'>Contact Me</p>
-                    <p>Email: Ajd62874@gmail.com</p>
-                    <p>Phone Number; 091289521</p>
+                    <p>Email: testeremail@gmail.com</p>
+                    <p>Phone Number: 09367612421</p>
                   <a href='https://drive.google.com/file/d/1EQC9lxwhRL9XyXoNkVSTB4tplc_es-cL/view?usp=drive_link'>
                   <button className='bg-yellow-500 p-2 rounded-md w-full'>Resume/CV</button>
                   </a>
